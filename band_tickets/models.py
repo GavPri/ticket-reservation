3adb82bdf121b92b52a3c36d482ae35c306172f7
GavPri <gavinprior5@gmail.com>
2023-07-17 03:21:00 +0000

    add str and name to gig

--- band_tickets/models.py
+++ band_tickets/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 class Gig(models.Models):
+    name = models.CharField(max_length=100)
     venue_name = models.CharField(max_length=100)
     date = models.DateField()
     time = models.TimeField()
+
+    def __str__(self):
+        return self.
     
